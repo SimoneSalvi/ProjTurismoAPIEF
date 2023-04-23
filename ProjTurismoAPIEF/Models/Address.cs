f03e7b0ea49e721f483f573345a5b79e13685a42
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-04-23 09:10:14 -0300

    Add project files.

---
+++ ProjTurismoAPIEF/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjTurismoAPIEF.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Stret { get; set; } //Logradouro
+        public string Neighborhood { get; set; } //Bairro
+        public int Number { get; set; } //Numero
+        public string ZipCode { get; set; } //CEP
+        public string Complement { get; set; }
+        public City City { get; set; }
+        public DateTime DtCadastre { get; set; } //Data_Cadastro
+
+    }
+}
---
+++ ProjTurismoAPIEF/Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjTurismoAPIEF.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DtCadastro { get; set; }
+    }
+}
